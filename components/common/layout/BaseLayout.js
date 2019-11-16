005080b67b63472756d5212559c5e80885befd84
Rakesh <rakeshkumar@myadvo.in>
2019-11-16 20:57:07 +0530

    Styling adjustments

--- components/common/layout/BaseLayout.js
+++ components/common/layout/BaseLayout.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import '../style/BaseStyle.scss';
+import Header from '../../Header/Header';
+import Nav from '../../Nav/Nav';
+import Banner from '../../Banner/Banner';
 
 const BaseLayout = (props) => {
     const { className, title, cannonical, children, metas = [] } = props;
@@ -7,7 +10,7 @@ const BaseLayout = (props) => {
         <>
             <Head>
                 <title>{title}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
                 <meta name="description" content="My name is Filip Jerga and I am an experienced software engineer and freelance developer. I have a Master's degree in Artificial Intelligence and several years of experience working on a wide range of technologies and projects from C++ development for ultrasound devices to modern mobile and web applications in React and Angular. Throughout my career, I have acquired advanced technical knowledge and the ability to explain programming topics clearly and in detail to a broad audience. I invite you to take my course, where I have put a lot of effort to explain web and software engineering concepts in a detailed, hands-on and understandable way." />
                 <meta name="keywords" content="jerga portfolio, jerga developer, jerga freelancig, jerga programming" />
                 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
@@ -16,13 +19,17 @@ const BaseLayout = (props) => {
                 <link rel="icon" type="image/ico" href="/static/favicon.ico" />
             </Head>
 
-            <div className="layout-base">
-                <main className={`cover ${className}`}>
-                    <div className="wrapper">
-                        {children}
-                    </div>
-                </main>
-            </div>
+
+            <main className={`${className}`}>
+                <Header>
+                    <Nav />
+                    <Banner />
+                </Header>
+                <div className="wrapper">
+                    {children}
+                </div>
+            </main>
+
         </>
     );
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,37 @@
 const withSass = require('@zeit/next-sass');
+const withImages = require('next-images')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = withSass({
+module.exports = withImages(withSass({
     webpack(config, { dev }) {
         if (config.mode === 'production') {
             if (Array.isArray(config.optimization.minimizer)) {
                 config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
             }
         }
+        // config.module.rules.push({
+        //     test: /\.svg$/,
+        //     use: ['@svgr/webpack'],
+        // });
+        config.module.rules.push({
+            test: /\.(svg|png|jpg|gif)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        })
+        config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: '[name].[ext]'
+                }
+            }
+        });
         return config;
     },
-});
+}));
