8c10f80d448fd45a10f230a37b42df97191ddf4e
Rakesh <rakeshkumar@myadvo.in>
2019-11-16 22:42:10 +0530

    bnner

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -5,7 +5,19 @@ const Banner = (props) => {
     return (
         <section>
             <div className="banner">
-
+                <div className="container">
+                    <div className="slide1">
+                        <div className="left"></div>
+                        <div className="content">
+                            <p>For Ethnics</p>
+                            <p>It is always difficult to select </p>
+                            <p>among various styles.</p>
+                            <p>We solve it for you.</p>
+                            <button className="btn">Shop Now</button>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
             </div>
         </section>
     );
